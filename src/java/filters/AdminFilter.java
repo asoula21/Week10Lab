60f0319134eacdb3a8a91e69969297892f9e0e83
asoula21 <anusone.soula@edu.sait.ca>
2022-11-13 21:37:24 -0700

    Add lab to git

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,41 @@
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import dataaccess.UserDB;
+import javax.servlet.http.HttpServletResponse;
+
+public class AdminFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {  
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+        
+        String email = (String) session.getAttribute("email");
+        
+        UserDB accessUsers = new UserDB();
+        if (!accessUsers.get(email).getRole().getRoleId().equals(1)) {
+            HttpServletResponse httpResponse = (HttpServletResponse)response;
+            httpResponse.sendRedirect("notes");
+            return;
+        }
+        
+        chain.doFilter(request, response);
+    }
+
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {        
+    }
+    
+    @Override
+    public void destroy() {        
+    }
+}
---
+++ web/WEB-INF/admin.jsp
@@ -0,0 +1,16 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Notes</title>
+    </head>
+    <body>
+        <h1>Welcome, ${email}</h1>
+        <h2>Administrator Panel</h2>
+        <p>Nothing here for now.</p>
+        <p>
+            <a href="login">Log out</a>
+        </p>
+    </body>
+</html>
